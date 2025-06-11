edfa1c5854d4fd38cc75952a574cf87084dff846
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2025-06-11 10:53:39 +0300

    feat: skeleton so far

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # javascript-github-actions
+
+## Actions
+- [hello-world](./hello-world) - A simple action that prints "Hello World" to the console.
+
+
+## Development
+
---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+import tsBaseConfig from '@map-colonies/eslint-config/ts-base';
+import { config } from '@map-colonies/eslint-config/helpers';
+
+export default config(tsBaseConfig, { ignores: ['new-action-template'] });
